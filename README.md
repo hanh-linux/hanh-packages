a72d2aa7b4b84d53bf210c2e889d4b7794177067
hanhlinux <conux2007@gmail.com>
2021-09-29 20:30:43 +0700

    Wed Sep 29 08:30:43 PM +07 2021

--- README.md
+++
@@ -1,2 +0,0 @@
-# pt-packages
-Project Tavorites build files
--- repo-update.sh
+++
@@ -1,6 +0,0 @@
-rm -rf date package-data.tar.gz
-echo $(date) > date
-tar -czvf package-data.tar.gz *
-git add .
-git commit -F date
-git push -u
