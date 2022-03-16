c8da4ade9a5b8f501c134037b6f4da1a5f27fb84
hanhlinux <conux2007@gmail.com>
2022-03-16 15:28:14 +0700

    Add some new packages

--- gendb.sh
+++ gendb.sh
@@ -1,15 +1,23 @@
 workdir=$(pwd)
 dirrepo=$*
 cd $dirrepo || exit 1 
+rm -rf $dirrepo/$dirrepo.database $workdir/repocon $dirrepo/$dirrepo.bad $dirrepo.lessbad
 mkdir -p $workdir/repocon
 for x in $(ls); do
 	dirnow=$(pwd)
 	mkdir -p $workdir/repocon/$x
+	if test -d $x; then
 	cd $x/ 
 	hanhbuild -g data || exit 1
 	mv data $workdir/repocon/$x/data || exit 1
 	cd $dirnow
+	fi
 done
+cd $workdir 
+
+for badfile in bad lessbad; do
+test -f $workdir/$dirrepo.$badfile && cp -r $workdir/$dirrepo.$badfile $workdir/repocon 
+done 
 
 cd $workdir/repocon
 tar -cjf $workdir/$(basename $dirrepo).database *
